acab0fb1b51070fa351e37d658b8b7dd33afd22a
Julien Rouhaud <julien.rouhaud@free.fr>
2022-05-19 18:10:08 +0800

    Add the newly introcued configure options --with-lz4 and --with-zstd.

--- postgres.pl
+++ postgres.pl
@@ -91,7 +91,10 @@ my %postgis_version=(
 
 # New configopts per version
 my %new_configopts_per_version=(
-        '11.0' => ['--with-llvm']);
+        '11.0' => ['--with-llvm'],
+        '14.0' => ['--with-lz4'],
+        '15.0' => ['--with-zstd'],
+);
 
 # No idea what version it could be, but lets's do this right now
 my %deprecated_configopts_per_version=(
